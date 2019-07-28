5901e0cbb34abbf0ffe35a882390c6090c81dd6e
JeannieFallon <jeanniefallon@gmail.com>
2019-07-28 12:48:52 -0400

    remove old java web app implementation

--- java/enums/Context.java
+++
@@ -1,8 +0,0 @@
-package com.metatarsal.primavera.enums;
-
-public enum Context {
-    HOME,
-    ROT13,
-    CAESAR,
-    VIGENERE
-}
--- java/enums/InstructionKeys.java
+++
@@ -1,8 +0,0 @@
-package com.metatarsal.primavera.enums;
-
-public enum InstructionKeys {
-    CHOOSE_CIPHER,
-    ENTER_PLAINTEXT,
-    ENTER_SHIFTVAL,
-    ENTER_KEY
-}
